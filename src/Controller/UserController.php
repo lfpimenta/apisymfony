769fb1cca19823da7ed8e2d7ba305b69f5b4f735
lfpimenta <lfpimenta@gmail.com>
2019-02-09 13:47:02 +0000

    remove routes fro yaml to anotations

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController {
+	/**
+	 * @Route("api/users", methods={"GET"})
+	 **/
 	public function list() {
 		return new Response("test");
 	}
 
+
+	/**
+	 * @Route("api/users/{id}", methods={"GET"})
+	 **/
+	public function display($id) {
+		return new Response("test display single $id record");
+	}
+
+	/**
+	 * @Route("api/users/", methods={"POST"})
+	 **/
 	public function store() {
 		return new Response("test store");
 	}
 
-	public function delete() {
+	/**
+	 * @Route("api/users/{id}", methods={"DELETE"})
+	 **/
+	public function delete($id) {
 		return new Response("test delete");
 	}
 }
